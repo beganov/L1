c34c6482c7e6d1119de45b9210975638a4ae2815
beganov <beganovdv@gmail.com>
2025-09-09 16:41:10 +0300

    L1.4 done

---
+++ L1.4/main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"os/signal"
+	"sync"
+	"syscall"
+)
+
+func worker(ctx context.Context, id int, wg *sync.WaitGroup) {
+	defer wg.Done()
+	for {
+		select {
+		case <-ctx.Done(): //вот тут вот обоснуй
+			fmt.Printf("Worker %d: shutting down\n", id)
+			return
+		default:
+			fmt.Printf("Worker %d: working...\n", id)
+		}
+	}
+}
+
+func main() {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	var wg sync.WaitGroup
+	numWorkers := 3
+	for i := 1; i <= numWorkers; i++ {
+		wg.Add(1)
+		go worker(ctx, i, &wg)
+	}
+	sigChan := make(chan os.Signal, 1)
+	signal.Notify(sigChan, syscall.SIGINT, syscall.SIGTERM)
+
+	select {
+	case sig := <-sigChan:
+		fmt.Printf("Caught signal: %s\n", sig)
+		cancel()
+	case <-ctx.Done():
+	}
+	wg.Wait()
+	fmt.Println("All workers shut down.")
+}
