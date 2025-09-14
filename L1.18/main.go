c50ca6ef0bb67bf0fbeb9850845273ff31a05aa5
beganov <beganovdv@gmail.com>
2025-09-14 23:26:41 +0300

    L1.18 done

---
+++ L1.18/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	"sync/atomic"
+)
+
+func main() {
+	var counter atomic.Int64
+	var wg sync.WaitGroup
+	counter.Store(0)
+	for i := 0; i < 1000; i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			counter.Add(1)
+		}()
+	}
+	wg.Wait()
+
+	fmt.Println(counter.Load())
+
+}
