3a8c22c3a18c85694d61a06b7447204b9e905f01
beganov <beganovdv@gmail.com>
2025-09-17 20:50:13 +0300

    L1.5 сommented

--- L1.5/main.go
+++ L1.5/main.go
@@ -6,9 +6,12 @@ import (
 	"time"
 )
 
-const (
-	n = 4
-)
+// Разработать программу, которая будет последовательно отправлять значения в канал,
+// а с другой стороны канала – читать эти значения. По истечении N секунд программа
+// должна завершаться.
+// Подсказка: используйте time.After или таймер для ограничения времени работы.
+
+var n = 4 * time.Second
 
 func worker(ch <-chan int, ctx context.Context, timeout <-chan time.Time) {
 	for data := range ch {
@@ -24,13 +27,13 @@ func worker(ch <-chan int, ctx context.Context, timeout <-chan time.Time) {
 }
 
 func main() {
-	ctx, cancel := context.WithTimeout(context.Background(), n*time.Second)
+	ctx, cancel := context.WithTimeout(context.Background(), n)
 	//как будто бы логично добавить таймаут в контекст
 	defer cancel()
 
 	ch := make(chan int)
 	timer := time.Now()
-	timeout := time.After(n * time.Second)
+	timeout := time.After(n)
 	//но в подсказке time.After, так что реализация с ним
 	go worker(ch, ctx, timeout)
 	counter := 0
