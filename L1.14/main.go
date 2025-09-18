6636f9082660b0db87afd1231070e732c4554903
beganov <beganovdv@gmail.com>
2025-09-18 14:00:34 +0300

    L1.14 commented

--- L1.14/main.go
+++ L1.14/main.go
@@ -5,7 +5,13 @@ import (
 	"strings"
 )
 
-type TestStruct struct {
+// L1.14
+// Разработать программу, которая в runtime способна определить тип переменной,
+// переданной в неё (на вход подаётся interface{}).
+// Типы, которые нужно распознавать: int, string, bool, chan (канал).
+// Подсказка: оператор типа switch v.(type) поможет в решении.
+
+type TestStruct struct { // для демонстрации работы с кастомными типами
 	intTest    int
 	stringTest string
 	boolTest   bool
@@ -20,6 +26,7 @@ func main() {
 		make(chan string),
 		make(chan bool),
 		make(chan TestStruct),
+		&TestStruct{},
 		3.14,
 		nil,
 	}
@@ -30,17 +37,19 @@ func main() {
 
 }
 func determineType(value interface{}) string {
-	switch value.(type) {
+	switch value.(type) { // оператор типа switch v.(type)
 	case int:
 		return "int"
 	case string:
 		return "string"
 	case bool:
 		return "bool"
-	case chan int, chan string, chan bool, chan interface{}: //не покрывает все канал, каждый канал - разный тип
+	case chan int, chan string, chan bool, chan interface{}:
+		//не покрывает все каналы, так как каждый канал - разный тип
 		return "chan"
 	default:
 		if strings.Split(fmt.Sprintf("%T", value), " ")[0] == "chan" {
+			// поэтому имеет место рассмотреть каналы через "%T"
 			return "chan"
 		}
 		return "unknown type"
