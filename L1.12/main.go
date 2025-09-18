b73ffd15679a56807f75ad2efeb537284b84ec1e
beganov <beganovdv@gmail.com>
2025-09-18 13:54:55 +0300

    L1.12 commented

--- L1.12/main.go
+++ L1.12/main.go
@@ -2,14 +2,23 @@ package main
 
 import "fmt"
 
-var array []string = []string{"cat", "cat", "dog", "cat", "tree"}
+var array = []string{"cat", "cat", "dog", "cat", "tree"}
+
+// L1.12
+// Имеется последовательность строк: ("cat", "cat", "dog", "cat", "tree").
+// Создать для неё собственное множество.
+// Ожидается: получить набор уникальных слов. Для примера, множество = {"cat", "dog", "tree"}.
 
 func main() {
 	var result []string
 	mapA := make(map[string]int)
+
+	// ищем уникальные значения и их количество
 	for _, i := range array {
 		mapA[i]++
 	}
+
+	// каждое найденное слово добавляем в выходное множество
 	for j := range mapA {
 		if mapA[j] != 0 {
 			result = append(result, j)
