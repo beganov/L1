383aa498f6831fa8f7d7e5bbf046b10f27375c18
beganov <beganovdv@gmail.com>
2025-09-18 14:30:16 +0300

    L1.18 commented

--- L1.18/main.go
+++ L1.18/main.go
@@ -6,17 +6,34 @@ import (
 	"sync/atomic"
 )
 
+// L1.18
+// Реализовать структуру-счётчик, которая будет инкрементироваться в конкурентной среде
+// (т.е. из нескольких горутин). По завершению программы структура должна выводить итоговое значение счётчика.
+// Подсказка: вам понадобится механизм синхронизации, например, sync.Mutex или sync/Atomic для безопасного инкремента.
+
+type Counter struct { // Структура-счётчик
+	counter atomic.Int64 //sync/Atomic для безопасного инкремента
+}
+
+func New() *Counter {
+	var Counter Counter
+	Counter.counter.Store(0)
+	return &Counter
+}
+
 func main() {
-	var counter atomic.Int64
+	Counter := New()
 	var wg sync.WaitGroup
-	counter.Store(0)
+	unsafeCounter := 0 //потоко небезопасный счетчик, для сравнения
 	for i := 0; i < 1000; i++ {
 		wg.Add(1)
 		go func() {
 			defer wg.Done()
-			counter.Add(1)
+			unsafeCounter++
+			Counter.counter.Add(1)
 		}()
 	}
 	wg.Wait()
-	fmt.Println(counter.Load())
+	fmt.Println(Counter.counter.Load(), unsafeCounter)
+	// unsafeCounter стабильно теряет процент-два, атомик - нет
 }
