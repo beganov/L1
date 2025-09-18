3603421593f9178653750b412975f3c52e224ddb
beganov <beganovdv@gmail.com>
2025-09-18 14:47:19 +0300

    L1.21 commented

--- L1.21/main.go
+++ L1.21/main.go
@@ -2,10 +2,19 @@ package main
 
 import "fmt"
 
-type existSystem struct {
+// L1.21 \
+// Реализовать паттерн проектирования «Адаптер» на любом примере.
+// Описание: паттерн Adapter позволяет сконвертировать интерфейс
+// одного класса в интерфейс другого, который ожидает клиент.
+// Продемонстрируйте на простом примере в Go: у вас есть существующий интерфейс
+// (или структура) и другой, несовместимый по интерфейсу потребитель
+// — напишите адаптер, который реализует нужный интерфейс и делегирует вызовы к встроенному объекту.
+// Поясните применимость паттерна, его плюсы и минусы, а также приведите реальные примеры использования.
+
+type existSystem struct { // у вас есть существующий интерфейс (или структура)
 }
 
-type clientInterface interface {
+type clientInterface interface { // и другой, несовместимый по интерфейсу потребитель
 	doSomethingElse() string
 }
 
@@ -17,12 +26,12 @@ func doSomethingElse() string {
 	return "Do something else"
 }
 
-type Adapter struct {
+type Adapter struct { // адаптер, который реализует нужный интерфейс
 	sys *existSystem
 }
 
 func (a Adapter) doSomethingElse() string {
-	return a.sys.doSomething()
+	return a.sys.doSomething() // и делегирует вызовы к встроенному объекту.
 }
 
 func main() {
