85b0f38ef3a15039968d611aa3fec8025c4aa0d8
beganov <beganovdv@gmail.com>
2025-09-18 15:06:07 +0300

    L1.24 commented

--- L1.24/main.go
+++ L1.24/main.go
@@ -5,16 +5,22 @@ import (
 	"math"
 )
 
-type Point struct {
-	x, y float64
+// L1.24
+// Разработать программу нахождения расстояния между двумя точками на плоскости.
+// Точки представлены в виде структуры Point с инкапсулированными (приватными) полями x, y (типа float64)
+//  и конструктором. Расстояние рассчитывается по формуле между координатами двух точек.
+// Подсказка: используйте функцию-конструктор NewPoint(x, y), Point и метод Distance(other Point) float64.
+
+type Point struct { // Точки представлены в виде структуры Point
+	x, y float64 //с инкапсулированными (приватными) полями x, y (типа float64)
 }
 
-func NewPoint(x, y float64) Point {
+func NewPoint(x, y float64) Point { // и конструктором
 	return Point{x: x, y: y}
 }
 
 func (p *Point) Distance(other Point) float64 {
-	return math.Sqrt(((p.x-other.x)*(p.x-other.x) + (p.y-other.y)*(p.y-other.y)))
+	return math.Sqrt(((p.x-other.x)*(p.x-other.x) + (p.y-other.y)*(p.y-other.y))) //Расстояние рассчитывается по формуле между координатами двух точек.
 }
 
 func main() {
